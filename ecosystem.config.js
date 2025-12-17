a61e3a45c4723fb0cf120e1570790c11fc78748a
HansM7 <melchorhans5@gmail.com>
2025-12-17 14:47:52 -0500

    feat: add changes

---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  apps: [
+    {
+      name: "gateway-service",
+      script: "cmd.exe",
+      args: "/c npm run start:dev",
+      cwd: "../upch-simulation-CLIENT-GATEWAY",
+      windowsHide: true,
+      env: {
+        PORT: 4000,
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "auth-service",
+      script: "cmd.exe",
+      args: "/c npm run start:dev",
+      cwd: "../upch-simulation-AUTH-MS",
+      windowsHide: true,
+      env: {
+        PORT: 4010,
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "stomatology-service",
+      script: "cmd.exe",
+      args: "/c npm run start:dev",
+      cwd: "../upch-simulation-STOMATOLOGY-MS",
+      windowsHide: true,
+      env: {
+        PORT: 4020,
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "stomatology-websocket",
+      script: "cmd.exe",
+      args: "/c npm run start:dev",
+      cwd: "../upch-simulation-STOMATOLOGY-WS",
+      windowsHide: true,
+      env: {
+        PORT: 5000,
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
