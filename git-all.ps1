9498386af8888b9985c0dce0819e701200d14cd7
HansM7 <melchorhans5@gmail.com>
2025-12-27 20:36:52 -0500

    feat: add script upload changes

---
+++ git-all.ps1
@@ -0,0 +1,37 @@
+﻿param (
+    [string]$CommitMessage = "feat: valid changes"
+)
+
+# Ruta donde está este script (config-app)
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Subir un nivel (workspace)
+$workspacePath = Resolve-Path "$scriptPath\.."
+
+Get-ChildItem -Path $workspacePath -Directory | ForEach-Object {
+    # Ignorar la carpeta config-app
+    if ($_.Name -eq "config-app") {
+        return
+    }
+
+    if (Test-Path "$($_.FullName)\.git") {
+        Write-Host "`nRepo: $($_.Name)" -ForegroundColor Cyan
+        
+        Push-Location $_.FullName
+        
+        $status = git status --porcelain
+        if (-not $status) {
+            Write-Host "Sin cambios" -ForegroundColor DarkGray
+            Pop-Location
+            return
+        }
+
+        git add .
+        git commit -m $CommitMessage
+
+        $branch = git branch --show-current
+        git push origin $branch
+
+        Pop-Location
+    }
+}
