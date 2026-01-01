57e3200358ef27303be915121c98906edf135fe7
hansm7 <melchorhans5@gmail.com>
2025-12-31 23:41:27 -0500

    feat: add app engineering

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -34,6 +34,17 @@ module.exports = {
       },
     },
     {
+      name: "engineering-service",
+      script: "cmd.exe",
+      args: "/c npm run start:dev",
+      cwd: "../upch-simulation-ENGINEERING-MS",
+      windowsHide: true,
+      env: {
+        PORT: 4030,
+        NODE_ENV: "development",
+      },
+    },
+    {
       name: "stomatology-websocket",
       script: "cmd.exe",
       args: "/c npm run start:dev",
